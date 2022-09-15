285e01ef285046a809a877226bdd4d6db191334c
Timileheen <111017742+Timileheen@users.noreply.github.com>
2022-09-15 21:53:19 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,10 @@
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
-}
+#include<stdio.h>  
+#include<conio.h>  
+int main()  
+{  
+int a;  
+printf("Enter an integer\n");  
+scanf("%d",&a);  
+printf("\n Enter an integer that you have entered %d",a);  
+return 0;  
+}  
